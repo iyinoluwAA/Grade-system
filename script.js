3a8155e0936c6dfae449fce10fe6917fa99efe16
iyinoluwAA <iyinoluwa150@gmail.com>
2024-03-12 07:59:29 +0100

    work almost done

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
-function gradeSystem() {
-     var userScore = Number(scoreOfuser.value)
-     console.log(userScore);
+function submit(){
+    var score = scoreOfuser.value
+    var songfail = new Audio ("")
+    var songpass = new Audio ("")
+    var songaverage = new Audio ("assests/audio_AUD-20200616-WA0019.mp3")
+    var  songcredit = new Audio ("assests/audio_AUD-20200617-WA0066.mp3")
+    var songgood = new Audio ("assests/mixkit-church-light-applause-501.wav")
+    var songexcellent =new Audio ("assests/audio_mixkit-ending-show-audience-clapping-478.wav")
+    if (scoreOfuser.value == "") {
+        show.innerHTML = "please enter your score"
+    }
+        else if (score >=0 && score <35){
+        show.innerHTML = `F-fail your score is  <b class = "text-warning fs-3">${score}</b>😐🤐`
+        songfail.play()   
+       } 
+        
+       else if (score >=35 && score <45){
+        show.innerHTML = `E-pass your score is  <b class = "text-warning fs-3">${score}</b>🥱🤨`
+        songpass.play()   
+       }
+        
+        else if (score >=45 && score <50){
+        show.innerHTML = `D-credit your score is  <b class = "text-warning fs-3">${score}</b>🦦😑`
+        songaverage.play() 
+    }
 
-     
-if (userScore >= 0 && userScore < 40) {
-    console.log("F-Fail");
-    Jordan.innerHTML = 'F-Fail'
-}
-}
+        else if (score >=50 && score <60){
+            show.innerHTML = `C-credit your score is  <b class = "text-warning fs-3">${score}</b>👀😑`
+            songcredit.play()
+        }
+         
+        else if (score >=60 && score < 70) {
+            show.innerHTML = `B-Good your score is <b class = "text-warning fs-3">${score}</b> 😁👍`
+            songgood.play()
+        }
+    
+        else if (score <= 100 && score >= 70 ) {
+        show.innerHTML = `A-Excellent your score is <b class = "text-success fs-3">${score}</b> 😍👌`
+        songexcellent.play()
+        }
+        scoreOfuser.value = ""
+    }
