d586c909b388c062773a21867f24a3ba5df5a120
iyinoluwAA <iyinoluwa150@gmail.com>
2024-03-12 17:08:44 +0100

    work dones

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function submit(){
     var score = scoreOfuser.value
-    var songfail = new Audio ("")
-    var songpass = new Audio ("")
+    var songfail = new Audio ("assests/audio_AUD-20200714-WA0013.m4a")
+    var songpass = new Audio ("assests/audio_AUD-20200621-WA0012 (1).m4a")
     var songaverage = new Audio ("assests/audio_AUD-20200616-WA0019.mp3")
     var  songcredit = new Audio ("assests/audio_AUD-20200617-WA0066.mp3")
     var songgood = new Audio ("assests/mixkit-church-light-applause-501.wav")
@@ -10,7 +10,7 @@ function submit(){
         show.innerHTML = "please enter your score"
     }
         else if (score >=0 && score <35){
-        show.innerHTML = `F-fail your score is  <b class = "text-warning fs-3">${score}</b>😐🤐`
+        show.innerHTML = `F-fail your score is  <b class = "text-danger fs-3">${score}</b>😐🤐`
         songfail.play()   
        } 
         
@@ -20,17 +20,17 @@ function submit(){
        }
         
         else if (score >=45 && score <50){
-        show.innerHTML = `D-credit your score is  <b class = "text-warning fs-3">${score}</b>🦦😑`
+        show.innerHTML = `D-credit your score is  <b class = "text-dark fs-3">${score}</b>🦦😑`
         songaverage.play() 
     }
 
         else if (score >=50 && score <60){
-            show.innerHTML = `C-credit your score is  <b class = "text-warning fs-3">${score}</b>👀😑`
+            show.innerHTML = `C-credit your score is  <b class = "text-info fs-3">${score}</b>👀😑`
             songcredit.play()
         }
          
         else if (score >=60 && score < 70) {
-            show.innerHTML = `B-Good your score is <b class = "text-warning fs-3">${score}</b> 😁👍`
+            show.innerHTML = `B-Good your score is <b class = "text-primary fs-3">${score}</b> 😁👍`
             songgood.play()
         }
     
